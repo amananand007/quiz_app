66b4b9404596f5dd32493afe5d4bc3b3b2a91d20
Aman Anand <amananand2751@gmail.com>
2025-07-07 08:47:02 +0530

    creating the add feature

---
+++ quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -0,0 +1,42 @@
+package com.telusko.quizapp.controller;
+
+import com.telusko.quizapp.Question;
+import com.telusko.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+        public List<Question> getAllQuestions() {
+
+            return questionService.getAllQuestions();
+        }
+
+    @GetMapping("category/{category}")
+        public List<Question> getQuestionByCategory(@PathVariable String category){
+            System.out.println(questionService.getQuestionsByCategory(category));
+            return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+ }
+
+
+
+
+
+
+
+
+
